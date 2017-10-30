ab2c1a55314f513697d2a448a74f01bdb93fab0e
Scott McKenna <Scott.McKenna@uk.ibm.com>
2017-10-30 22:00:16 +0000

    Logging Request Body

--- controller/BalanceController.js
+++ controller/BalanceController.js
@@ -5,8 +5,7 @@ var BalanceService = require("../service/BalanceService");
 router.post("/", getBalance);
 
 function getBalance(request, response) {
-  console.log(request.url);
-  console.log(request.path);
+  console.log(request.body);
   BalanceService.getEthereumBalance("1234").then((data) => {
     response.status(200).send(data);
   });
