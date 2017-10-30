5d35042be48f86ea3d4da1960e47e45063c5a697
Scott McKenna <Scott.McKenna@uk.ibm.com>
2017-10-30 22:25:12 +0000

    Parse Int of result amount

--- controller/BalanceController.js
+++ controller/BalanceController.js
@@ -8,7 +8,7 @@ function getBalance(request, response) {
   console.log(request.body);
   BalanceService.getEthereumBalance(request.body.text).then((data) => {
     var responseMsg = "Currency: Ethereum\n";
-    responseMsg += "Amount: " + data.amount / 1000000 + "ETH";
+    responseMsg += "Amount: " + parseInt(data.result, 10) / 1000000 + "ETH";
     response.status(200).send(responseMsg);
   });
 }
